e4bd6bb313daa2a71fef8bb908204c856d9358ed
TahaThinks <tahaa4587@gmail.com>
2024-04-26 01:40:25 +0400

    Refactor Code

--- data.py
+++ data.py
@@ -1,4 +1,10 @@
 import requests
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+    "category": 18
+}
 
-request = requests.get(url="https://opentdb.com/api.php?amount=10&category=18&type=boolean")
+request = requests.get(url="https://opentdb.com/api.php", params=parameters)
+request.raise_for_status()
 question_data = request.json()["results"]
