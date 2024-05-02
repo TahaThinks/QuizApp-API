d2981678dc9b473d79c5a3faac502d5a97d66d4f
TahaThinks <tahaa4587@gmail.com>
2024-05-02 18:03:22 +0400

    Add Canvas

--- ui.py
+++ ui.py
@@ -7,14 +7,17 @@ class QuizInterface:
         self.window.title("Quizzler by Taha")
         self.window.config(bg=THEME_COLOR)
 
+        self.canvas = Canvas(width=300, height=250)
+        self.canvas.grid(row=0, column=0, columnspan=2)
+
         photo1 = PhotoImage(file="images/true.png")
         photo2 = PhotoImage(file="images/false.png")
 
         self.button1 = Button(image=photo1)
         self.button2 = Button(image=photo2)
 
-        self.button1.grid(row=0, column=0)
-        self.button2.grid(row=0, column=1)
+        self.button1.grid(row=1, column=0)
+        self.button2.grid(row=1, column=1)
 
 
 
